066fe2483980aa2c0d1e88520c168b592642cec0
minjongbaek <minjongbaek@gmail.com>
2022-05-24 23:29:14 +0900

    feat: 아이템 드래그 앤 드랍 기능 추가

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ export default function Todos({ $app, initialState }) {
     this.render = () => {
         this.$target.innerHTML = `
             <input type="text" id="add-item-input" placeholder="아이템 추가">
-            ${this.state.map(({id, title}) => `
-            <li data-id="${id}">
+            ${this.state.map(({ id, title }) => `
+            <li data-id="${id}" class='to-do-item' draggable="true">
                 ${title}
                 <button class='delete-btn'>아이템 삭제</button>
             </li>`).join('')}
@@ -26,17 +26,41 @@ export default function Todos({ $app, initialState }) {
 
     this.setEvent = () => {
         // 아이템 추가 인풋에 click 이벤트 리스너 부여
-        this.$target.querySelector('#add-item-input').addEventListener('keyup', ({key, target}) => {
+        this.$target.querySelector('#add-item-input').addEventListener('keyup', ({ key, target }) => {
             if (key !== 'Enter') return
-            this.setState([...this.state, {id: new Date().getTime(), title: `${target.value}`}])
+            this.setState([...this.state, { id: new Date().getTime(), title: `${target.value}` }])
         })
 
         // 아이템 삭제 버튼에 click 이벤트 리스너 부여
         this.$target.querySelectorAll('.delete-btn').forEach(deleteBtnDOM => {
-            deleteBtnDOM.addEventListener('click', ({ target }) => {
-                const state = [...this.state].filter(item => Number(target.parentNode.dataset.id) !== item.id)
+            deleteBtnDOM.addEventListener('click', (event) => {
+                const state = [...this.state].filter(item => Number(event.target.parentNode.dataset.id) !== item.id)
                 this.setState(state)
             })
+        })
+
+        this.$target.querySelectorAll('.to-do-item').forEach(liDOM => {
+            liDOM.addEventListener('dragstart', (event) => {
+                const targetIndex = [...this.state].findIndex(({ id }) => id === Number(event.target.dataset.id))
+                event.dataTransfer.setData('itemIndex', targetIndex)
+            })
+            liDOM.addEventListener('dragover', (event) => {
+                event.preventDefault()
+            })
+        })
+
+        this.$target.addEventListener('drop', (event) => {
+            event.preventDefault()
+
+            const state = [...this.state]
+            const itemIndex = Number(event.dataTransfer.getData('itemIndex'))
+            const targetIndex = state.findIndex(({id}) => id === Number(event.target.dataset.id))
+
+            const tmpTarget = state[targetIndex]
+
+            state[targetIndex] = state[itemIndex]
+            state[itemIndex] = tmpTarget
+            this.setState(state)
         })
     }
 
